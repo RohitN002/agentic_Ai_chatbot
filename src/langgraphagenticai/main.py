8b79d78d3b44387cd78238c27277e4ba7a05db35
RohitN002 <rohitrandy002@gmail.com>
2025-08-17 12:45:11 +0530

    intergrating the entire pipeline with the frontend

--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
-
+from src.langgraphagenticai.LLMS.groqllm import GroqLLM
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+from src.langgraphagenticai.ui.streamlitui.display_result import DisplayResultStreamlit
 
 def load_langgraph_agnticai_app():
 
@@ -11,4 +13,25 @@ def load_langgraph_agnticai_app():
         st.error("Error : FAILED TO LOAD USER INPUT FROM THE UI")
         return
 
-    user_messages= st.chat_input("Enter yuur message here")    
+    user_message= st.chat_input("Enter yuur message  here")    
+
+   if user_message:
+    try:
+        obj_llm_config=GroqLLM(user_controls_input=user_input)
+        model=obj_llm_config.get_ll_model()
+        if not model:
+            st.error("LLM nodel could not be initialized ")
+        usercase = user_input.get("selected_usecase")
+        if not usecase:
+            st.error("Error: no usecase selected")
+            return 
+        graph_builder=GraphBuilder(model)
+        try:
+            graph= graph_builder.setup_graph(usecase)
+            DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+        except Exception as e:
+            st.error(f"Error graph setup failed -{e}") 
+            return
+
+        except Exception as e:      
+            st.error(f"Error : Graph setup failed {e}")
